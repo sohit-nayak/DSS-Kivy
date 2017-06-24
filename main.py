8208b23d0305916b5ecfa1d7cb8270481c5409d6
sohit-nayak <sohit.nayak@gmail.com>
2017-06-24 15:11:50 +0530

    roc graph done

--- main.py
+++ main.py
@@ -117,93 +117,111 @@ class GraphPopup(Popup):
 		self.auto_dismiss = True
 
 	def view_graph(self, *args):
-		# Run classifier with cross-validation and plot ROC curves
-		X = self.root.data.drop(self.root.ids.predict_dropdown_choose_parameter.text, axis=1)
-		y = self.root.data[self.root.ids.predict_dropdown_choose_parameter.text]
-		classes = y.unique()
-		print classes
-		y = label_binarize(y, classes=[0,1,2])
-		n_classes = y.shape[1]
+		# # Run classifier with cross-validation and plot ROC curves
+		# X = self.root.data.drop(self.root.ids.predict_dropdown_choose_parameter.text, axis=1)
+		# y = self.root.data[self.root.ids.predict_dropdown_choose_parameter.text]
+		# classes = y.unique()
+		# print classes
+		# y = label_binarize(y, classes=[0,1,2])
+		# n_classes = y.shape[1]
 
-		# Add noisy features to make the problem harder
-		random_state = np.random.RandomState(0)
-		n_samples, n_features = X.shape
-		X = np.c_[X, random_state.randn(n_samples, 200 * n_features)]
+		# # Add noisy features to make the problem harder
+		# random_state = np.random.RandomState(0)
+		# n_samples, n_features = X.shape
+		# X = np.c_[X, random_state.randn(n_samples, 200 * n_features)]
 
-		X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.5, random_state=0)
-		classifier = OneVsRestClassifier(self.root.model)
-		y_score = classifier.fit(X_train, y_train).decision_function(X_test)
-		# Compute ROC curve and ROC area for each class
-		fpr = dict()
-		tpr = dict()
-		roc_auc = dict()
-		for i in range(n_classes):
-		    fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
-		    roc_auc[i] = auc(fpr[i], tpr[i])
+		# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.5, random_state=0)
+		# classifier = OneVsRestClassifier(self.root.model)
+		# y_score = classifier.fit(X_train, y_train).decision_function(X_test)
+		# # Compute ROC curve and ROC area for each class
+		# fpr = dict()
+		# tpr = dict()
+		# roc_auc = dict()
+		# for i in range(n_classes):
+		#     fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
+		#     roc_auc[i] = auc(fpr[i], tpr[i])
 
 
-		# Compute micro-average ROC curve and ROC area
-		fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel())
-		roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+		# # Compute micro-average ROC curve and ROC area
+		# fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel())
+		# roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
 
+		# # plt.figure()
+		# lw = 2
+		# # plt.plot(fpr[2], tpr[2], color='darkorange', lw=lw, label='ROC curve (area = %0.2f)' % roc_auc[2])
+		# # # plt.plot(fpr[2], tpr[2], color='darkorange',
+		# # #          lw=lw, label='ROC curve (area = %0.2f)' % roc_auc[2])
+		# # plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+		# # plt.xlim([0.0, 1.0])
+		# # plt.ylim([0.0, 1.05])
+		# # plt.xlabel('False Positive Rate')
+		# # plt.ylabel('True Positive Rate')
+		# # plt.title('Receiver operating characteristic example')
+		# # plt.legend(loc="lower right")
+		# # plt.show()
+		# # First aggregate all false positive rates
+		# all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+
+		# # Then interpolate all ROC curves at this points
+		# mean_tpr = np.zeros_like(all_fpr)
+		# for i in range(n_classes):
+		#     mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+
+		# # Finally average it and compute AUC
+		# mean_tpr /= n_classes
+
+		# fpr["macro"] = all_fpr
+		# tpr["macro"] = mean_tpr
+		# roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+		# # Plot all ROC curves
 		# plt.figure()
-		lw = 2
-		# plt.plot(fpr[2], tpr[2], color='darkorange', lw=lw, label='ROC curve (area = %0.2f)' % roc_auc[2])
-		# # plt.plot(fpr[2], tpr[2], color='darkorange',
-		# #          lw=lw, label='ROC curve (area = %0.2f)' % roc_auc[2])
-		# plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+		# plt.plot(fpr["micro"], tpr["micro"],
+		#          label='micro-average ROC curve (area = {0:0.2f})'
+		#                ''.format(roc_auc["micro"]),
+		#          color='deeppink', linestyle=':', linewidth=4)
+
+		# plt.plot(fpr["macro"], tpr["macro"],
+		#          label='macro-average ROC curve (area = {0:0.2f})'
+		#                ''.format(roc_auc["macro"]),
+		#          color='navy', linestyle=':', linewidth=4)
+
+		# colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
+		# for i, color in zip(range(n_classes), colors):
+		#     plt.plot(fpr[i], tpr[i], color=color, lw=lw,
+		#              label='ROC curve of class {0} (area = {1:0.2f})'
+		#              ''.format(i, roc_auc[i]))
+
+		# plt.plot([0, 1], [0, 1], 'k--', lw=lw)
 		# plt.xlim([0.0, 1.0])
 		# plt.ylim([0.0, 1.05])
 		# plt.xlabel('False Positive Rate')
 		# plt.ylabel('True Positive Rate')
-		# plt.title('Receiver operating characteristic example')
+		# plt.title('Some extension of Receiver operating characteristic to multi-class')
 		# plt.legend(loc="lower right")
-		# plt.show()
-		# First aggregate all false positive rates
-		all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+		# # plt.show()
 
-		# Then interpolate all ROC curves at this points
-		mean_tpr = np.zeros_like(all_fpr)
-		for i in range(n_classes):
-		    mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+		# # print plt.gcf().axes
+		X = self.root.data.drop(self.root.ids.predict_dropdown_choose_parameter.text, axis=1)
+		y = self.root.data[self.root.ids.predict_dropdown_choose_parameter.text]
+		X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.33, random_state=69)
+		self.root.model.fit(X_train, y_train)
+		probs = self.root.model.predict_proba(X_test)
+		preds = probs[:,1]
+		fpr, tpr, threshold = roc_curve(y_test, preds)
+		roc_auc = auc(fpr, tpr)
 
-		# Finally average it and compute AUC
-		mean_tpr /= n_classes
-
-		fpr["macro"] = all_fpr
-		tpr["macro"] = mean_tpr
-		roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
-
-		# Plot all ROC curves
-		plt.figure()
-		plt.plot(fpr["micro"], tpr["micro"],
-		         label='micro-average ROC curve (area = {0:0.2f})'
-		               ''.format(roc_auc["micro"]),
-		         color='deeppink', linestyle=':', linewidth=4)
-
-		plt.plot(fpr["macro"], tpr["macro"],
-		         label='macro-average ROC curve (area = {0:0.2f})'
-		               ''.format(roc_auc["macro"]),
-		         color='navy', linestyle=':', linewidth=4)
-
-		colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
-		for i, color in zip(range(n_classes), colors):
-		    plt.plot(fpr[i], tpr[i], color=color, lw=lw,
-		             label='ROC curve of class {0} (area = {1:0.2f})'
-		             ''.format(i, roc_auc[i]))
-
-		plt.plot([0, 1], [0, 1], 'k--', lw=lw)
-		plt.xlim([0.0, 1.0])
-		plt.ylim([0.0, 1.05])
-		plt.xlabel('False Positive Rate')
+		plt.title('Receiver Operating Characteristic')
+		plt.plot(fpr, tpr, 'b', label = 'AUC = %0.2f' % roc_auc)
+		plt.legend(loc = 'lower right')
+		plt.plot([0, 1], [0, 1],'r--')
+		plt.xlim([0, 1])
+		plt.ylim([0, 1])
 		plt.ylabel('True Positive Rate')
-		plt.title('Some extension of Receiver operating characteristic to multi-class')
-		plt.legend(loc="lower right")
+		plt.xlabel('False Positive Rate')
 		# plt.show()
-
-		# print plt.gcf().axes
+		print 'done'
 		self.ids.graph_area.add_widget(FigureCanvasKivyAgg(plt.gcf()))
-
 
 class ManualInputPopup(Popup):
 	
@@ -363,7 +381,10 @@ class RootWidget(TabbedPanel):
 		
 		graph_display = self.ids.graph_display
 		graph_display.clear_widgets()
+		plt.close()
+		plt.clf()
 		graph = type_graph.text
+		# graph_display.add_widget(FigureCanvasKivyAgg(plt.close()))
 		sns.set_palette('colorblind')
 		
 		if graph == 'Count Plot':
@@ -422,7 +443,7 @@ class RootWidget(TabbedPanel):
 			self.update_the_status = 'Plots data and a linear regression model fit.'
 
 		elif graph == 'Kde Plot':
-			g = sns.FacetGrid(self.data, col=hue.text, row="survived")
+			g = sns.FacetGrid(self.data, col=hue.text, row=hue.text)
 			g.map(sns.kdeplot, X.text, Y.text)
 			plt.xticks(rotation='vertical')
  			print plt.gcf().axes
@@ -479,6 +500,8 @@ class RootWidget(TabbedPanel):
 		self.prediction()
 		print self.model
 		self.prediction()
+		# self.X = self.data.drop(self.ids.predict_dropdown_choose_parameter.text, axis=1)
+		# self.y = self.data[self.ids.predict_dropdown_choose_parameter.text]
 
 		self.model.fit(self.data.drop(self.ids.predict_dropdown_choose_parameter.text, axis=1), self.data[self.ids.predict_dropdown_choose_parameter.text])
 		accuracy = cross_val_score(self.model, self.data.drop(self.ids.predict_dropdown_choose_parameter.text, axis=1), self.data[self.ids.predict_dropdown_choose_parameter.text], cv=5, scoring='accuracy').mean()
@@ -507,7 +530,7 @@ class RootWidget(TabbedPanel):
 			tol = float(params['tol'])
 			coef0 = float(params['coef0'])
 
-			self.model = SVC(C=C, kernel=kernel, degree=degree, tol=tol, coef0=coef0)
+			self.model = SVC(C=C, kernel=kernel, degree=degree, tol=tol, coef0=coef0, probability=True)
 
 		if classifier_type == 'ANN':
 			hidden_layer_sizes = tuple(params['hidden_layer_sizes'])
